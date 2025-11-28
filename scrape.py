5017842a690962eaf1a0c7287788a5dda2b58818
srihari2903 <srihari.a2903@gmail.com>
2025-11-27 22:58:36 -0500

    Update scrape file

--- scrape.py
+++ scrape.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.safari.webdriver import WebDriver as Safari
 from selenium.webdriver.safari.options import Options
+from bs4 import BeautifulSoup
 import time
 
 def scrape_website(website):
@@ -9,7 +10,7 @@ def scrape_website(website):
     options = webdriver.ChromeOptions()
     driver = webdriver.Chrome(service = Service(chrome_driver_path), options= options)'''
 
-    options = Options()
+    '''options = Options()
     options.use_technology_preview = False
 
     driver = Safari(options = options)
@@ -22,4 +23,36 @@ def scrape_website(website):
 
         return html
     finally:
-        driver.quit()
+        driver.quit()'''
+    options = Options()
+    driver = Safari(options = options)
+    
+    driver.get(website)
+    print("Navigated! Scraping page content...")
+    html = driver.page_source
+    return html
+
+def extract_body_content(html_content):
+    soup = BeautifulSoup(html_content, "html.parser")
+    body_content = soup.body
+    if body_content:
+        return str(body_content)
+    return ""
+
+def clean_body_content(body_content):
+    soup = BeautifulSoup(body_content, "html.parser")
+
+    for script_or_style in soup(["script", "style"]):
+        script_or_style.extract()
+
+    cleaned_content = soup.get_text(separator = "\n")
+    cleaned_content = "\n".join(
+        line.strip() for line in cleaned_content.splitlines() if line.strip()
+    )
+
+    return cleaned_content
+
+def split_dom_content(dom_content, max_length = 6000):
+    return [
+        dom_content[i: i+max_length] for i in range(0, len(dom_content), max_length)
+    ]
